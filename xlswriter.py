bf81970d7660e3c841ec7cb1ea9b8ff5114a21b5
jybhaha <jybhaha@gmail.com>
2016-10-19 20:17:59 +0800

    'first'

---
+++ xlswriter.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# **********************************************************
+# * Author        : jiao
+# * Email         : jybhaha@gmail.com
+# * Create time   : 2016-10-19 16:02
+# * Last modified : 2016-10-19 16:02
+# * Filename      : xlswriter.py
+# * Description   : 
+# **********************************************************
+
+from openpyxl.reader.excel import load_workbook
+from openpyxl.worksheet.protection import SheetProtection
+
+
+filename='test.xlsx'
+sheetRowMaxLength=1101
+listColumn=['A','B','C','D','E','F','G']
+
+workbook = load_workbook(filename=filename)
+sheetnames=workbook.get_sheet_names()
+worksheet = workbook.get_sheet_by_name(sheetnames[0])
+
+tempRow=1
+for i in listColumn:
+	for j in range(1,sheetRowMaxLength):
+		if(worksheet[i+str(j)].value == None):
+			if(j != sheetRowMaxLength-1):
+				tempRow=tempRow+1
+			else:
+				worksheet.unmerge_cells(i+str(j-tempRow)+':'+i+str(j))
+				for k in range(0,tempRow):
+					worksheet[i+str(j-k)].value=worksheet[i+str(j-tempRow)].value
+		else:
+			if(tempRow!=1):
+				print('\n********\n'+'unmerging:  '+i+str(j-tempRow)+'  to   '+i+str(j-1))
+				try:
+					worksheet.unmerge_cells(i+str(j-tempRow)+':'+i+str(j-1))
+				except BaseException:
+					tempRow=1
+					continue
+				for k in range(0,tempRow-1):
+					worksheet[i+str(j-k-1)].value=worksheet[i+str(j-tempRow)].value
+				tempRow=1
+			else:
+				continue
+print('ending')
+workbook.save('test2.xlsx')
+
+
